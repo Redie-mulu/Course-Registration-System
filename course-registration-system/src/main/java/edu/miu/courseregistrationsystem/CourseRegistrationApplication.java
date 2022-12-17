7ae0cf251d77ed7b2b74a90f4547969a7c0f52a9
REDIET WOLDEMARIAM <redietwoldemariam@REDIETs-MacBook-Pro.local>
2022-12-17 11:01:46 -0600

    added more end points for student

--- course-registration-system/src/main/java/edu/miu/courseregistrationsystem/CourseRegistrationApplication.java
+++ course-registration-system/src/main/java/edu/miu/courseregistrationsystem/CourseRegistrationApplication.java
@@ -5,6 +5,7 @@ import edu.miu.courseregistrationsystem.entity.Address;
 import edu.miu.courseregistrationsystem.entity.Student;
 import edu.miu.courseregistrationsystem.repository.AddressRepository;
 import edu.miu.courseregistrationsystem.repository.StudentRepository;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(scanBasePackages = {"edu.miu.courseregistrationsystem"})
 @EntityScan(basePackages = {"edu.miu.courseregistrationsystem"})
 @EnableJpaRepositories(basePackages = {"edu.miu.courseregistrationsystem"})
+@OpenAPIDefinition
 public class CourseRegistrationApplication implements CommandLineRunner {
 	@Autowired
 	private AddressRepository addressRepository;
--- course-registration-system/src/main/java/edu/miu/courseregistrationsystem/controller/StudentController.java
+++ course-registration-system/src/main/java/edu/miu/courseregistrationsystem/controller/StudentController.java
@@ -3,13 +3,13 @@ package edu.miu.courseregistrationsystem.controller;
 import edu.miu.courseregistrationsystem.dto.StudentDto;
 import edu.miu.courseregistrationsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
-
     @Autowired
     private StudentService studentService;
 
@@ -22,6 +22,21 @@ public class StudentController {
     public ResponseEntity<?> registerStudent(@RequestBody StudentDto studentDto) {
         studentService.registerStudent(studentDto);
         return new ResponseEntity<>(studentDto, null, 200);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getStudent(@PathVariable long id) {
+        StudentDto studentDto = studentService.getStudent(id);
+        return new ResponseEntity<>(studentDto, null, 200);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateStudent(@PathVariable long id, @RequestBody StudentDto studentDto) {
+        studentService.updateStudent(id, studentDto);
+        return new ResponseEntity<StudentDto>(studentDto, HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> dropStudent(@PathVariable long id) {
+        studentService.dropStudent(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
