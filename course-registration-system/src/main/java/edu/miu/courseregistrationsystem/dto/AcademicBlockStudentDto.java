2cd15b295206cf3ce00b1846f1f86ac22ff0327e
redietwoldemariam <mulunehrediet@gmail.com>
2022-12-22 08:21:13 -0600

    edit the code

--- course-registration-system/src/main/java/edu/miu/courseregistrationsystem/dto/AcademicBlockStudentDto.java
+++ course-registration-system/src/main/java/edu/miu/courseregistrationsystem/dto/AcademicBlockStudentDto.java
@@ -1,7 +1,9 @@
 package edu.miu.courseregistrationsystem.dto;
 
-import edu.miu.courseregistrationsystem.entity.CourseOffering;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
